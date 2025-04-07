cd164359ff4f3f9a23a0fc7822df65dd4968ad46
Adriano <122603292+Adriano542@users.noreply.github.com>
2025-04-07 11:56:48 +0200

    s

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const mainTitle = document.querySelector('h1')
 const btn = document.querySelector('button')
+const changeColorInp = document.querySelector('changeColorInp')
 
 function changeColor(kolor){
     mainTitle.style.color=kolor
 }
 
 btn.addEventListener('click', function(){
-    changeColor('red')
+    console.log(changeColorInp.value)
+    changeColor(changeColorInp.value)
     this.style.backgroundColor = '#ff0'
 })

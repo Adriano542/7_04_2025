8d44e73c21608207a77dabade0825bc89243afb5
Adriano <122603292+Adriano542@users.noreply.github.com>
2025-04-07 12:08:44 +0200

    d

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const mainTitle = document.querySelector('h1')
 const btn = document.querySelector('button')
-const changeColorInp = document.querySelector('changeColorInp')
+const changeColorInp = document.querySelector('#changeColorInp')
 const textInput = document.querySelector('#textInput')
 
 function changeColor(kolor){
@@ -11,5 +11,5 @@ btn.addEventListener('click', function(){
     changeColor(changeColorInp.value)
     this.style.backgroundColor = '#ff0'
 
-    
+
 })
